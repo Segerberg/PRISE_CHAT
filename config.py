ae5087593b83990060c483141572c80d7828c0c4
segerberg <andreassegerberg@gmail.com>
2020-11-02 19:25:10 +0100

    Update sqlite path

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir,'files', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_APP_BRAND = os.environ.get('FLASK_APP_BRAND')
